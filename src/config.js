2db63e3cff4614f14514f1393651de86eb355ea6
malun <malun666@126.com>
2016-06-30 18:40:06 +0800

    ss

---
+++ src/config.js
@@ -0,0 +1,13 @@
+/*================================
+=            app的配置文          =
+================================*/
+
+module.exports = {
+  isDebug: true, // 配置是否是调试
+
+  // 配置mogondb的链接字符串
+  mongodbConStr: "mongodb://localhost/blog_db",
+
+  //网站监听的端口 
+  webport: 3000
+};
