43450b05c4ad3633b26e3cb7614ece5d801b169f
malun <malun666@126.com>
2016-07-01 12:24:56 +0800

    edit the config for mongodb

--- src/config.js
+++ src/config.js
@@ -6,8 +6,8 @@ module.exports = {
   isDebug: true, // 配置是否是调试
 
   // 配置mogondb的链接字符串
-  mongodbConStr: "mongodb://localhost:29097/blog_db",
+  mongodbConStr: "mongodb://localhost:27097/blog_db",
 
   //网站监听的端口 
-  webport: 3000
+  webport: 80
 };
